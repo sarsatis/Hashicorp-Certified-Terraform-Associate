c80dbc046283bc6dc12f59c6e34ce82966639f8f
sarsatis <sarthak8055@gmail.com>
2024-08-22 23:54:49 +0100

    test

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-02c21308fed24a8ab" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
